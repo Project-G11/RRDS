0be8a3ce755bf06a052be29903cfb1350dba03d3
Luna-Blumberg <luna.blumberg@finaps.nl>
2023-09-12 23:04:56 +0200

    added initial keyword matching model

--- baselines.py
+++ baselines.py
@@ -3,11 +3,11 @@ class MajorityClassModel:
     # initialize model with training labels
     def __init__(self, labels):
         # set majority class as the label that has the highest occurence in the list
-        self.__majorityClass = max(labels,key=labels.count)
+        self.majorityClass = max(labels,key=labels.count)
 
     # assigns majority class label to input data
     def test(self, input):
-        return self.__majorityClass
+        return self.majorityClass
     
     # evaluates model based on lists of test data and labels 
     def evaluate(self, data, labels):
@@ -23,3 +23,19 @@ class MajorityClassModel:
         # return accuracy
         return correct / len(data)
         
+# model that assigns labels to data based on keyword matching rules
+class KeywordMatchingModel(MajorityClassModel):
+    def __init__(self, labels):
+        super().__init__(labels)
+
+    def test(self, input):
+        if "bye" in input:
+            return "bye"
+        elif "thank" in input:
+            return "thankyou"
+        elif "hi " in input or "hello" in input or "helo " in input:
+            return "hello"
+        else: 
+            return self.majorityClass
+    
+    
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from sklearn.model_selection import train_test_split
-from baselines import MajorityClassModel
+from baselines import MajorityClassModel, KeywordMatchingModel
 
 # open data file
 txt = open("dialog_acts.dat").readlines()
@@ -14,10 +14,13 @@ for line in txt:
 # split data in test and training data
 insts_train, insts_test, labels_train, labels_test = train_test_split(insts, labels, test_size=0.15, random_state=42)
 
-# get the mojority class baseline model
+# get baseline models
 mc_model =  MajorityClassModel(labels_train)
+km_model =  KeywordMatchingModel(labels_train)
 
-# get accuracy
+# get accuracies
 mc_acc = mc_model.evaluate(insts_test, labels_test)
 print("Majority class model accuracy is", mc_acc)
 
+km_acc = km_model.evaluate(insts_test, labels_test)
+print("Keyword matching model accuracy is", km_acc)
