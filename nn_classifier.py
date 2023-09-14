cb6022f21a2edcffe1461cbb89b9be88c27b76e7
Christos <christosp1@hotmail.com>
2023-09-14 23:29:12 +0200

    Changed the classifier to work with pretrained tokenizer

--- nn_classifier.py
+++ nn_classifier.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.layers import Dense, Embedding, Flatten
@@ -6,6 +7,8 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.optimizers import Adam  
 import matplotlib.pyplot as plt
+from transformers import BertTokenizer, BertModel
+
 
 class NNClassifier:
     
@@ -19,22 +22,32 @@ class NNClassifier:
         # All the unique names of the labels(classes). They are 15
         classes = np.unique(labels_train)
         # Convert all the arrays in numpy arrays to manipulate the data easier
-        insts_train = np.array(insts_train)
-        insts_test = np.array(insts_test)
+        
         labels_train = np.array(labels_train)
         labels_test = np.array(labels_test)
-        # Tokenize each sentence and add padding based on the max_words per sentence.
-        tokenizer = Tokenizer(num_words=max_words, oov_token='<OOV>')
-        tokenizer.fit_on_texts(insts_train)
-        
-        sequences_train = tokenizer.texts_to_sequences(insts_train)
-        X_train = pad_sequences(sequences_train, maxlen=max_words)
-        
-        sequences_test = tokenizer.texts_to_sequences(insts_test)
-        X_test = pad_sequences(sequences_test, maxlen=max_words)
         
         #-----------------------------------------------------------------
-        def map_labels(labels_train):
+        # Tokenize each sentence and add padding based on the max_words per sentence.
+        # tokenizer = Tokenizer(num_words=max_words, oov_token='<OOV>')
+        # tokenizer.fit_on_texts(insts_train)
+        
+        # sequences_train = tokenizer.texts_to_sequences(insts_train)
+        # X_train = pad_sequences(sequences_train, maxlen=max_words)
+        
+        # sequences_test = tokenizer.texts_to_sequences(insts_test)
+        # X_test = pad_sequences(sequences_test, maxlen=max_words)
+        
+        def tokenize_sentences(list,max_words):
+            tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+            token_ids = [tokenizer.encode(sentence, max_length=max_words, truncation=True, padding='max_length') for sentence in list]
+            return np.array(token_ids)
+            
+        X_train = tokenize_sentences(insts_train,max_words)
+        X_test = tokenize_sentences(insts_test,max_words)
+
+        
+       
+        def map_labels(labels_train,length):
             unique_labels = np.unique(labels_train)
             label_mapping={}
             i=0
@@ -42,17 +55,16 @@ class NNClassifier:
                 label_mapping[label] = i
                 i+=1
             labels_train_int = [label_mapping[label] for label in labels_train]
-            return labels_train_int
+            one_hot_labels = keras.utils.to_categorical(labels_train_int, num_classes=length)
+            return one_hot_labels
 
-        labels_train_int = map_labels(labels_train)
-        y_train = keras.utils.to_categorical(labels_train_int, num_classes=len(classes))
+        y_train = map_labels(labels_train,len(classes))        
+        y_test = map_labels(labels_test,len(classes))
         
-        labels_test_int = map_labels(labels_test)
-        y_test = keras.utils.to_categorical(labels_test_int, num_classes=len(classes))
-
+        
         #-----------------------------------------------------------------
         model = keras.Sequential()
-        model.add(Embedding(input_dim=max_words, output_dim=50, input_length=max_words))
+        model.add(Embedding(input_dim=X_train.max()+1, output_dim=50, input_length=max_words))
         model.add(Flatten())
         model.add(Dense(64, activation='relu'))
         # model.add(keras.layers.Dropout(0.25))
@@ -64,7 +76,7 @@ class NNClassifier:
     
     
     def train(self, model, X_train, y_train,  X_test, y_test):
-        history = model.fit(X_train, y_train, epochs=12, batch_size=8, validation_data=(X_test, y_test))
+        history = model.fit(X_train, y_train, epochs=12, batch_size=32, validation_data=(X_test, y_test))
         self.evaluation(X_train, y_train, model, history)
         
     def evaluation(self, X_train, y_train, model, history):
